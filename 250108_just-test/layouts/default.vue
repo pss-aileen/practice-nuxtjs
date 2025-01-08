c83983b9fbd7e84800b2fd9a02657dfd237dd0ed
Aileen <140990100+pss-aileen@users.noreply.github.com>
2025-01-08 14:56:09 +0900

    ルーティング変更

---
+++ 250108_just-test/layouts/default.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <header>
+      <h1>page title</h1>
+
+      <nav>
+        <ul>
+          <li><NuxtLink to="/">HOME</NuxtLink></li>
+          <li><NuxtLink to="/about">ABOUT</NuxtLink></li>
+          <li><NuxtLink to="/contact">CONTACT</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: #eee;
+  padding: 16px;
+  h1 {
+    font-size: 16px;
+  }
+
+  nav {
+    ul {
+      list-style: none;
+      padding: 0;
+      display: block;
+      font-size: 14px;
+      margin: 0;
+
+      li {
+        display: inline-block;
+      }
+
+      li + li {
+        margin-left: 16px;
+      }
+    }
+  }
+}
+</style>
