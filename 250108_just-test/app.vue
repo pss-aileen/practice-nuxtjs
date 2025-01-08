a0e8fb545b3cb7dfc84cd5d2bdfd9381cc5ada6c
Aileen <140990100+pss-aileen@users.noreply.github.com>
2025-01-08 14:13:45 +0900

    routing 1

--- 250108_just-test/app.vue
+++ 250108_just-test/app.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <NuxtLink to="/">HOME</NuxtLink>
+    <NuxtLink to="/about">ABOUT</NuxtLink>
+    <NuxtLink to="/contact">CONTACT</NuxtLink>
     <NuxtPage />
   </div>
 </template>
